ceeac2f8078ce82b4d60dbf77d2c30c850648c1a
NoahMenezes <2006noahmenezes@gmail.com>
2025-09-13 23:09:59 +0530

    First Push

---
+++ src/components/ChannelListContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ChannelList, useChatContext } from 'stream-chat-react'
+
+import Cookies from 'universal-cookie'
+import { ChannelSearch} from 'stream-chat-react'
+export default function ChannelListContainer() {
+  return (
+    <div>
+      ChannelListContainer
+    </div>
+  )
+}
